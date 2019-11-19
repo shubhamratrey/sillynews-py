932b3b95a98e41a5825649cc86d0ec701dd6e65c
Shubh <shubhamratrey@gmail.com>
2019-11-19 16:45:48 +0530

    fixes

--- sillynews/helpers/insta_helper.py
+++ sillynews/helpers/insta_helper.py
@@ -102,7 +102,6 @@ class InstaHelper(object):
     @staticmethod
     def profile_page_recent_posts(profile_url):
         results = []
-
         try:
             response = ScrapperHelper.get_response_from_url(profile_url)
             json_data = ScrapperHelper.extract_json_data_from_html(response)
