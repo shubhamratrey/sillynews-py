d214133597fe59231c3a5a69a0787341ecc7e5fa
Shubh <shubhamratrey@gmail.com>
2019-11-30 19:56:33 +0530

    time fixes

--- sillynews/helpers/task_helper.py
+++ sillynews/helpers/task_helper.py
@@ -8,7 +8,7 @@ class TaskHelper(object):
     @staticmethod
     def get_schedules(profile_id, page_no=1, page_size=20):
         schedule_list = []
-        schedules = Schedule.objects.filter(profile_id=profile_id).order_by('-time')
+        schedules = Schedule.objects.filter(profile_id=profile_id).order_by('-start_time')
         paginator = Paginator(schedules, page_size)
         has_next = page_no < paginator.num_pages
 
